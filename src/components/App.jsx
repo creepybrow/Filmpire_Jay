485617cb4edc0952d44a94aabc616cf391aee77c
Jay <creepybrow@juno.com>
2025-01-22 14:42:40 -0800

    first commit for filmpire

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
-
+import React from 'react';
+import { CssBaseline } from '@mui/material';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import useStyles from './styles';
+import {Actors, MovieInformation, Movies, NavBar, Profile} from './'
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+    const classes = useStyles();
+    return(
+    <div className={classes.root}>
+    <CssBaseline />
+    <NavBar/>
+    <main className={classes.content}>
+        <div className={classes.toolbar}/>
+      <Routes>
+        {/* Use the element prop to pass JSX elements */}
+        <Route path="/movie/:id" element={<MovieInformation/>} />
+        <Route path="/actors/:id" element={<Actors/>} />
+        <Route path="/" element={<Movies/>} />
+        <Route path="/profile/:id" element={<Profile/>} />
+      </Routes>
+    </main>
+  </div>
+    )
+};
 
-export default App
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';  // Use the correct import for React 18
-
+import{BrowserRouter} from 'react-router-dom';
 import App from './components/App';
 
 // Create a root and render the app
 const root = ReactDOM.createRoot(document.getElementById('root')); 
-root.render(<App />);
+root.render(
+
+<BrowserRouter>
+<App />
+</BrowserRouter>,
+);
