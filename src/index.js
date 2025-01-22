4c68ea8c26cca22a46eaaf57077053b7d9c264b1
Jay <creepybrow@juno.com>
2025-01-22 12:54:59 -0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';  // Use the correct import for React 18
+
+import App from './components/App';
+
+// Create a root and render the app
+const root = ReactDOM.createRoot(document.getElementById('root')); 
+root.render(<App />);
